f982274d67e5908d647d31e87ac598653280ed67
wachka06 <wachka06@gmail.com>
2018-08-28 19:33:50 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const newDriver = {...driver}; //spread operater
+  newDriver[key] = value;
+  return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  const newDriver = driver;
+  newDriver[key] = value;
+  return newDriver;
+}
+
+function deleteFromDriverByKey(driver, key){
+  const newDriver = {...driver};
+  delete newDriver[key];
+  return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+  const newDriver = driver;
+  delete newDriver[key];
+  return newDriver;
+}
